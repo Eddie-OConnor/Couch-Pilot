0861d808c3903f0982b91323810114b771a2c4aa
Eddie-OConnor <eddie.oconnor3@gmail.com>
2024-01-27 15:43:56 -0500

    tweaks to vite for netlify

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 // vite.config.js
 
-import {defineConfig} from 'vite'
-
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  esbuild: {
     target: 'es2020',
-	plugins: []	
-})
+  },
+  plugins: [],
+});
