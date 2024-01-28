540eaf9a8d7caad99bbb2f093232b7f0f91d7d13
Eddie-OConnor <eddie.oconnor3@gmail.com>
2024-01-28 10:00:17 -0500

    Update fetchApi.js

--- netlify/functions/fetchApi/fetchApi.js
+++ netlify/functions/fetchApi/fetchApi.js
@@ -4,25 +4,48 @@
 import OpenAI from 'openai';
 import { createClient } from "@supabase/supabase-js";
 
+const handler = async (event) => {
+    try {
+        const keys = {
+            openaiApiKey: process.env.OPENAI_API_KEY,
+            supabaseApiKey: process.env.SUPABASE_API_KEY,
+            supabaseUrl: process.env.SUPABASE_URL,
+            omdbApiKey: process.env.OMDB_API_KEY
+        }
+    //   return {
+    //     statusCode: 200,
+    //     body: JSON.stringify({ 
+    //       openai: keys.openaiApiKey,
+    //       supabaseApiKey: keys.supabaseApiKey,
+    //       supabaseUrl: keys.supabaseUrl,
+    //       omdbApiKey: keys.omdbApiKey
+    //      }),
+    //   }
+        const instances = initializeApiInstances(keys)
+        return instances
+    } catch (error) {
+      return { statusCode: 500, body: error.toString() }
+    }
+  }
 
-export async function initializeApiInstances(){
+export async function initializeApiInstances(keys){
     try {
         /* OpenAI config */
-        if (process.env.OPENAI_API_KEY) throw new Error("OpenAI API key is missing or invalid.");
+        if (keys.openaiApiKey) throw new Error("OpenAI API key is missing or invalid.");
         const openai = new OpenAI({
-            apiKey: process.env.OPENAI_API_KEY,
+            apiKey: keys.openaiApiKey,
             dangerouslyAllowBrowser: true
         });
 
         /* Supabase config */
-        const supabaseApiKey = process.env.SUPABASE_API_KEY
+        const supabaseApiKey = keys.supabaseApiKey
         if (!supabaseApiKey) throw new Error(`Supabase API key is missing or invalid`);
-        const supabaseUrl = process.env.SUPABASE_URL;
+        const supabaseUrl = keys.supabaseUrl;
         if (!supabaseUrl) throw new Error(`Supabase URL is missing or invalid`);
         const supabase = createClient(supabaseUrl, supabaseApiKey);
 
         /* OMDb API config */
-        const omdbApiKey = process.env.OMDB_API_KEY
+        const omdbApiKey = keys.omdbApiKey
         if(!omdbApiKey) throw new Error('OMDb API key is missing or invalid')
 
         return {openai, supabase, omdbApiKey}
@@ -31,20 +54,6 @@ export async function initializeApiInstances(){
     }
 }
 
-const handler = async (event) => {
-  try {
-        const instances = await initializeApiInstances()
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ 
-        openai: instances.openai,
-        supabase: instances.supabase,
-        omdbApiKey: instances.omdbApiKey
-       }),
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
-}
+
 
 module.exports = { handler }
