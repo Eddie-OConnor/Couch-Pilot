2e0436d07706b4e0653579449af7eaf31b34a4b1
Eddie-OConnor <eddie.oconnor3@gmail.com>
2024-01-28 12:05:30 -0500

    updating vite for multiple html

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 // vite.config.js
 
 import {defineConfig} from 'vite'
-
+import {resolve} from 'path'
 
 export default defineConfig({
     build: {
         target: "esnext",
+        rollupOptions: {
+            input: {
+              main: resolve(__dirname, 'index.html'),
+              otherPage: resolve(__dirname, 'results.html'),
+            },
+          },
       },
 	plugins: []	
 })
